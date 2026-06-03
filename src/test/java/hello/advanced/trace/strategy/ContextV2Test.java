359d8bff927b595d6081a1f83c7cbce9045369a5
Lee-0210 <tjsans9069@gmail.com>
2026-06-04 00:22:06 +0900

    feat: 템플릿 콜백 패턴 적용

--- src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -20,8 +20,8 @@ public class ContextV2Test {
     @Test
     void strategyV1() {
         ContextV2 context = new ContextV2();
-        context.exectue(new StrategyLogic1());
-        context.exectue(new StrategyLogic2());
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
     }
 
     /**
@@ -31,13 +31,13 @@ public class ContextV2Test {
     void strategyV2() {
         ContextV2 context = new ContextV2();
 
-        context.exectue(new Strategy() {
+        context.execute(new Strategy() {
             @Override
             public void call() {
                 log.info("비즈니스 로직1 실행");
             }
         });
-        context.exectue(new Strategy() {
+        context.execute(new Strategy() {
             @Override
             public void call() {
                 log.info("비즈니스 로직2 실행");
@@ -52,7 +52,7 @@ public class ContextV2Test {
     void strategyV3() {
         ContextV2 context = new ContextV2();
 
-        context.exectue(() -> log.info("비즈니스 로직1 실행"));
-        context.exectue(() -> log.info("비즈니스 로직2 실행"));
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
     }
 }
